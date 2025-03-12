e83980dc15124df0fd85af35b89b9ab26ea528c5
Ken Sia <kenneth.sia@cloudemployee.co.uk>
2025-03-13 01:23:07 +0800

    add lifecycle hook on registration create

--- src/api/registration/content-types/registration/lifecycles.ts
+++ src/api/registration/content-types/registration/lifecycles.ts
@@ -18,22 +18,32 @@ export default {
       console.log(registration);
 
       // Send confirmation email
+      // await strapi.plugins["email"].services.email.send({
+      //   to: registration.user.email,
+      //   subject: `Registration Confirmation - ${registration.event.title}`,
+      //   html: `
+      //     <h1>Registration Confirmation</h1>
+      //     <p>Dear ${registration.user.firstName},</p>
+      //     <p>Thank you for registering for ${registration.event.title}.</p>
+      //     <p>Registration Details:</p>
+      //     <ul>
+      //       <li>Event: ${registration.event.title}</li>
+      //       <li>Amount: ${registration.amount}</li>
+      //     </ul>
+      //     <p>You can view your registration status in your <a href="https://kick2pedal.net/dashboard">member portal</a></p>
+      //     <p>We will review your registration and update you once it's confirmed.</p>
+      //     <p>Best regards,<br>Kick2Pedal Team</p>
+      //   `,
+      // });
       await strapi.plugins["email"].services.email.send({
         to: registration.user.email,
-        subject: `Registration Confirmation - ${registration.event.title}`,
-        html: `
-          <h1>Registration Confirmation</h1>
-          <p>Dear ${registration.user.firstName},</p>
-          <p>Thank you for registering for ${registration.event.title}.</p>
-          <p>Registration Details:</p>
-          <ul>
-            <li>Event: ${registration.event.title}</li>
-            <li>Amount: ${registration.amount}</li>
-          </ul>
-          <p>You can view your registration status in your <a href="https://kick2pedal.net/dashboard">member portal</a></p>
-          <p>We will review your registration and update you once it's confirmed.</p>
-          <p>Best regards,<br>Kick2Pedal Team</p>
-        `,
+        // from: "no-reply@cms.kick2pedal.net", //e.g. single sender verification in SendGrid
+        // cc: "valid email address",
+        // bcc: "valid email address",
+        // replyTo: "valid email address",
+        subject: "The Strapi Email plugin worked successfully",
+        text: "Hello world!",
+        html: "Hello world!",
       });
     } catch (error) {
       console.error("Error sending registration confirmation email:", error);
